213bd8fbe9fa15561aec47199c4397b2e9bc9c55
Darren Ford <dford_au-reg@yahoo.com>
2020-07-09 08:49:54 +1000

    Using Framework instead

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CIFilterFactory",
+			type: .dynamic,
             targets: ["CIFilterFactory"]),
     ],
     dependencies: [
